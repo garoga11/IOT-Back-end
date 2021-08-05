bbc0b7698a425b3c7c5efacb5d6edb1e2f336cf7
unknown <a6519150007@utch.edu.mx>
2021-08-05 16:47:17 -0600

    Creating endpoint to get user balance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, json, jsonify, request
+from flask import Flask, jsonify, request
+from flask.wrappers import Response
 from flask_restful import Api, Resource, abort
 from flask_cors import CORS
 import db_config as database
@@ -17,8 +18,11 @@ def get():
     token = request.args.get("abc")
 
     key = f"Hi9@yBl4$j8WM91*4Wf8{user_id}{money}"
-    key_str= str(key)
-    key_str = blake2b(digest_size=64).hexdigest()
+    key_ard = hashlib.blake2b(key.encode())
+    key_str = key_ard.hexdigest()
+
+    print(key_str)
+    print(token)
 
     if token == key_str:
         _request= database.db.passenger_transactions.find_one({"_id":user_id})
@@ -56,6 +60,15 @@ def get():
     else:
         return jsonify({"message":"Something went wrong, try again"})
 
+
+@app.route('/getUser/', methods=['GET'])
+def getUser():
+
+    user_id = str(request.args.get("user_id"))
+    _request= database.db.passenger_transactions.find_one({"_id":user_id})
+    response = json_util.dumps(_request)
+    return Response(response)
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
